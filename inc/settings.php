c8fdd4ebca4acf1a968eb9568b22b3276b5eb0cd
Jim <jim@noideersoftware.co.uk>
2021-09-12 15:44:36 +0100

    add chunk size setting

--- inc/settings.php
+++ inc/settings.php
@@ -1,7 +1,8 @@
 <?php
 $settings['OAUTH_APP_KEY' ]= 'orweskt13n9fe7r';
 $settings['OAUTH_APP_SECRET'] = 'gaho8bp2yotbudr';
-$settings['OAUTH_REFRESH_TOKEN'] ='0kVF5NVixnoAAAAAAAAAAYw4ckOtdy6eZKgZGdvVgoDnho5WzlDZMjDDb2L-wJgF';
+$settings['OAUTH_REFRESH_TOKEN'] ='uRuWKQ8A1WcAAAAAAAAAAWSHI0zoC6KWEkDxmGX8oZ1oLK1YbnlS0UF1EMOBAcuq';
 $settings['DATE_FORMAT'] =  'd-m-y';
 $settings['FILE_RETAIN'] = 2;
 $settings['DEBUG'] = 1;
+$settings['CHUNK_SIZE'] = 100;
