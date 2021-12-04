de12351d5f3b81cbeac3e4ecdaba90c6ebfb05ad
Jim <jim@noideersoftware.co.uk>
2021-12-04 15:21:32 +0000

    update settings.php

--- inc/settings.php
+++ inc/settings.php
@@ -1,7 +1,7 @@
 <?php
-$settings['OAUTH_APP_KEY' ]= 'orweskt13n9fe7r';
-$settings['OAUTH_APP_SECRET'] = 'gaho8bp2yotbudr';
-$settings['OAUTH_REFRESH_TOKEN'] ='uRuWKQ8A1WcAAAAAAAAAAWSHI0zoC6KWEkDxmGX8oZ1oLK1YbnlS0UF1EMOBAcuq';
+$settings['OAUTH_APP_KEY' ]= '';
+$settings['OAUTH_APP_SECRET'] = '';
+$settings['OAUTH_REFRESH_TOKEN'] ='';
 $settings['DATE_FORMAT'] =  'd-m-y';
 $settings['FILE_RETAIN'] = 2;
 $settings['DEBUG'] = 1;
